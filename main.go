ed9f2c3689566af945a797c4fb7ea9fbf7c4729d
dubs11kt <dubs11kt@gmail.com>
2022-06-29 22:08:41 +0900

    add: health check handler

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 type myHandler struct{}
 type thisHandler struct{}
+type healthcheck struct{}
 
 func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "foo")
@@ -17,9 +18,14 @@ func (h *thisHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "bar")
 }
 
+func (h *healthcheck) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
+}
+
 func main() {
 	http.Handle("/foo", &myHandler{})
 	http.Handle("/bar", &thisHandler{})
+	http.Handle("/healthcheck", &healthcheck{})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
